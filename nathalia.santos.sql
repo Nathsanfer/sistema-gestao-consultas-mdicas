ec0262e1d35f9082649e3fde442e81515ed91f8d
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2024-11-05 16:37:52 -0300

    feat: Create inserts table "medicos"

--- nathalia.santos.sql
+++ nathalia.santos.sql
@@ -10,5 +10,18 @@ nome VARCHAR(100) NOT NULL,
 especialidade VARCHAR(100) NOT NULL
 );
 
+-- Inserindo dados na tabela de médicos --
+
+INSERT INTO medicos (nome, especialidade) VALUES 
+(‘Nathalia Santos Ferreira’, ‘Pediatra’),
+(‘Fernanda Alves Louro’, ‘Clínico Geral’),
+(‘Samuel dos Santos Braga’, ‘Cardiologista’),	
+(‘Alejandra Taiman Barros’, ‘Ortopedista’),
+(‘Bruna Nascimento Savelli’, ‘Neurologista’),
+(‘Felipe Santos Dev’, ‘Psiquiatra’),
+(‘Eduardo Correia’, ‘Dermatologista’),
+(‘Marcelo Carboni’, ‘Oncologista’),
+(‘Thiago Rodrigues’, ‘Anestesiologista’);
+
 
 
