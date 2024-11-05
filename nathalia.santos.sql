13d39c02bd40c193d09a0673d098e0024980e853
Nathsanfer <158168917+Nathsanfer@users.noreply.github.com>
2024-11-05 16:36:12 -0300

    feat: Create table "Médicos"

--- nathalia.santos.sql
+++ nathalia.santos.sql
@@ -2,6 +2,13 @@
 
 CREATE DATABASE clinica;
 
+-- Criando a tabela de médicos --
+
+CREATE TABLE medicos (
+id_medico SERIAL PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+especialidade VARCHAR(100) NOT NULL
+);
 
 
 
